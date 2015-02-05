c6be573eac031ae0b32ce41a87a63a2d286a9c48
Ryan Dinesman <rdinesman@gmail.com>
2015-02-05 14:33:18 -0600

    Added more style to TODO List

--- public/todo_list.php
+++ public/todo_list.php
@@ -5,19 +5,34 @@
 <html>
 	<head>
 		<title>TODO List</title>
+		<link href='http://fonts.googleapis.com/css?family=Oswald:400,700' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="/css/site.css">
 	</head>
 	<body>
-		<h1>TODO List</h1>
+		<h1>
+			<em>
+				TODO List
+			</em>
+		</h1>
 		<ul>
 			<li>Write this page</li>
 			<li>Finish today's class</li>
 			<li>Write up notes</li>
 			<li>Go home</li>
 		</ul>
-		<form method="POST" action="/todo_list.php">
-			<h2>Add Item to TODO List</h2>
-			<label for="todo_add"><input type="text" id="todo_add" name="todo_add" placeholder="Add item"></label>
-			<button type="submit">Add</button>
-		</form>
+		<div id="todo_add">
+			<form method="POST" action="/todo_list.php">
+				<h2>
+					Add Item to TODO List
+				</h2>
+				<label for="todo_add">
+					TODO:
+				</label>
+				<input type="text" class="todo_add" name="todo_add" placeholder="Task">
+				<button type="submit">
+					Add
+				</button>
+			</form>
+		</div>
 	</body>
 </html>
