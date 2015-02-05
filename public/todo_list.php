01156d923b7aa65a7436cdcd6a1fb88d2628b3a6
Ryan Dinesman <rdinesman@gmail.com>
2015-02-05 16:35:42 -0600

    Added pink sides and worked on center formatting

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,6 +1,8 @@
+<!--
 <?php
 	var_dump($_POST);
 ?>
+-->
 
 <html>
 	<head>
@@ -9,30 +11,32 @@
 		<link rel="stylesheet" type="text/css" href="/css/site.css">
 	</head>
 	<body>
-		<h1>
-			<em>
-				TODO List
-			</em>
-		</h1>
-		<ul>
-			<li>Write this page</li>
-			<li>Finish today's class</li>
-			<li>Write up notes</li>
-			<li>Go home</li>
-		</ul>
-		<div id="todo_add">
-			<form method="POST" action="/todo_list.php">
-				<h2>
-					Add Item to TODO List
-				</h2>
-				<label for="todo_add">
-					TODO:
-				</label>
-				<input type="text" class="todo_add" name="todo_add" placeholder="Task">
-				<button type="submit">
-					Add
-				</button>
-			</form>
-		</div>
+			<div class="box" id="left_box"></div>
+			<div class="box" id="right_box"></div>
+			<h1 class="header">
+				<em>
+					TODO List
+				</em>
+			</h1>
+			<ul>
+				<li>Write this page</li>
+				<li>Finish today's class</li>
+				<li>Write up notes</li>
+				<li>Go home</li>
+			</ul>
+			<div id="todo_add">
+				<form method="POST" action="/todo_list.php">
+					<h2 class="header">
+						Add Item to TODO List
+					</h2>
+					<label for="todo_add">
+						TODO:
+					</label>
+					<input type="text" class="todo_add" name="todo_add" placeholder="Task">
+					<button type="submit">
+						Add
+					</button>
+				</form>
+			</div>
 	</body>
 </html>
